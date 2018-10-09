1aecf529e52c0b1efd90f84adf8693d6f552ca16
Ketan Patel <pketan@narola.local>
2018-10-09 10:10:19 +0530

    local setup and Convert css to scss

--- src/app/shared/config.ts
+++ src/app/shared/config.ts
@@ -1,6 +1,6 @@
 import timezones from 'google-timezones-json';
 export class Config {
-    apiURL = 'http://localhost:2220/';
+    apiURL = 'http://192.168.100.42:8080/';
     signType = ['RGB', 'GRAYSCALE'];
     userRole = ['ROLE_USER', 'ROLE_ADMIN'];
     timeZone = timezones;
