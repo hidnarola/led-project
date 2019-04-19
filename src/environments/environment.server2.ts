b1018dd923d0711fcea5623d8f5cd7b917f22e43
Pratik Shah <pratik.shah@ekzero.com>
2019-04-19 12:22:14 +0530

    - Dhwanil : Set display service url

--- src/environments/environment.server2.ts
+++ src/environments/environment.server2.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiURL: 'https://led-signer-lb-1407974322.us-east-2.elb.amazonaws.com/led-signer/api'
+    apiURL: 'https://www.digitaldisplayservice.com/api'
 };
